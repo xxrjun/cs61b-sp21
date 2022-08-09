0c8d8dd4b2db62e9d6c68710c8df91bcdbb9c8be
rjun <rjun0729@gmail.com>
2022-08-09 17:38:30 +0800

    Fixed all bugs in lab2/DebugExercise.

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -6,33 +6,20 @@ package DebugExercise;
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
+    /**
+     *  Fixed bugs.
+     */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        return a > b ? a : b;
     }
 
 
     /** Returns the sum of a and b. Do not step into this function. */
+    /**
+     *  Fixed bugs.
+     */
     public static int add(int a, int b) {
-        int x = a, y = b;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        return a + b;
     }
 
     /** Returns a new array where entry i is the max of
@@ -54,11 +41,14 @@ public class DebugExercise2 {
     }
 
     /** Returns the sum of all elements in x. */
+    /**
+     *  Fixed the bugs.
+     */
     public static int arraySum(int[] x) {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
