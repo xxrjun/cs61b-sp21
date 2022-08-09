14e3d3531e894a5c7c2fab5921950a6fc0124caa
rjun <rjun0729@gmail.com>
2022-08-09 16:36:06 +0800

    done lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -2,7 +2,7 @@ public class HelloNumbers {
     public static void main(String[] args) {
         int x = 1;
         int total = 0;
-        while (x <= 11) {
+        while (x < 11) {
             System.out.print(total + " ");
             total = total + x;
             x = x + 1;
