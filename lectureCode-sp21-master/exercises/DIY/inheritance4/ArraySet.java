ee2be8b5055764c4f340e1350f56d2a125156ae6
rjun <rjun0729@gmail.com>
2022-09-02 06:34:56 +0800

    Finished DIY `ArraySet.java`.

--- lectureCode-sp21-master/exercises/DIY/inheritance4/ArraySet.java
+++ lectureCode-sp21-master/exercises/DIY/inheritance4/ArraySet.java
@@ -1,23 +1,41 @@
 public class ArraySet<T> {
 
+    private T[] items;
+    private int size;
+
     public ArraySet() {
+        items = (T[]) new Object[100];
+        size = 0;
     }
 
     /* Returns true if this map contains a mapping for the specified key.
      */
     public boolean contains(T x) {
+        for (int i = 0; i < size(); i++) {
+            // for null case
+            if (items[i] == null) {
+                if (x == null) {
+                    return true;
+                }
+            } else if (items[i].equals(x)) {
+                return true;
+            }
+        }
         return false;
     }
 
     /* Associates the specified value with the specified key in this map. 
        Throws an IllegalArgumentException if the key is null. */
     public void add(T x) {
-        return;
+        if (!contains(x)) {
+            items[size] = x;
+            size += 1;
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     public int size() {
-        return 0;
+        return size;
     }
 
     public static void main(String[] args) {
@@ -26,9 +44,10 @@ public class ArraySet<T> {
         s.add("horse");
         s.add("fish");
         s.add("house");
-        s.add("fish");        
-        System.out.println(s.contains("horse"));        
-        System.out.println(s.size());       
+        s.add("fish");
+        System.out.println(s.contains("horse"));
+        System.out.println(s.contains(null));
+        System.out.println(s.size());
     }
 
     /* Also to do:
