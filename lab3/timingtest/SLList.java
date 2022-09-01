36dc6d7c859a37acd7b719fed2dfe4ffe15b1e48
rjun <rjun0729@gmail.com>
2022-09-01 17:44:49 +0800

    Makes SLList in lab3 to be more efficient.

--- lab3/timingtest/SLList.java
+++ lab3/timingtest/SLList.java
@@ -6,9 +6,11 @@ public class SLList<Item> {
 	private class IntNode {
 		public Item item;
 		public IntNode next;
+		public IntNode prev;
 
-		public IntNode(Item i, IntNode n) {
+		public IntNode(IntNode p, Item i, IntNode n) {
 			item = i;
+			prev = p;
 			next = n;
 		}
 	}
@@ -19,20 +21,30 @@ public class SLList<Item> {
 
 	/** Creates an empty timingtest.SLList. */
 	public SLList() {
-		sentinel = new IntNode(null, null);
+		sentinel = new IntNode(null, null, null);
 		size = 0;
 	}
 
 	public SLList(Item x) {
-		sentinel = new IntNode(null, null);
-		sentinel.next = new IntNode(x, null);
+		sentinel = new IntNode(null, null, null);
+		sentinel.next = new IntNode(sentinel, x, sentinel);
 		size = 1;
 	}
 
 	/** Adds x to the front of the list. */
 	public void addFirst(Item x) {
-		sentinel.next = new IntNode(x, sentinel.next);
-		size = size + 1;
+		if(this.isEmpty()){
+			IntNode newNode = new IntNode(sentinel, x, sentinel);
+			sentinel.next = newNode;
+			sentinel.prev = newNode;
+		} else {
+			IntNode curFirst = sentinel.next;
+			IntNode newNode = new IntNode(sentinel, x, sentinel.next);
+			sentinel.next = newNode;
+			curFirst.prev = newNode;
+		}
+
+		size += 1;
 	}
 
 	/** Returns the first item in the list. */
@@ -42,34 +54,33 @@ public class SLList<Item> {
 
 	/** Adds x to the end of the list. */
 	public void addLast(Item x) {
-		size = size + 1;
-
-		IntNode p = sentinel;
-
-		/* Advance p to the end of the list. */
-		while (p.next != null) {
-			p = p.next;
+		if(this.isEmpty()){
+			IntNode newNode = new IntNode(sentinel, x, sentinel);
+			sentinel.next = newNode;
+			sentinel.prev = newNode;
+		} else {
+			IntNode curLast = sentinel.prev;
+			IntNode newNode = new IntNode(sentinel.prev, x, sentinel);
+			sentinel.prev = newNode;
+			curLast.next = newNode;
 		}
 
-		p.next = new IntNode(x, null);
+		size += 1;
 	}
 
 	/** returns last item in the list */
 	public Item getLast() {
-		IntNode p = sentinel;
-
-		/* Advance p to the end of the list. */
-		while (p.next != null) {
-			p = p.next;
-		}
-
-		return p.item;
+		return sentinel.prev.item;
 	}
 
 
 	/** Returns the size of the list. */
 	public int size() {
 		return size;
+	}
+
+	public boolean isEmpty(){
+		return size() == 0;
 	}
 
 	public static void main(String[] args) {
