985b0b07045c66718562be00d518a79c7e4f123f
rjun <rjun0729@gmail.com>
2022-09-02 18:12:55 +0800

    Fixed code style problems in `TimeADeque.java`.

--- proj1/timingtest/TimeADeque.java
+++ proj1/timingtest/TimeADeque.java
@@ -5,11 +5,12 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Stopwatch;
 
 public class TimeADeque {
-    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+    private static void printTimingTable(AList<Integer> ns, AList<Double> times,
+                                         AList<Integer> opCounts) {
         System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
         System.out.printf("------------------------------------------------------------\n");
-        for (int i = 0; i < Ns.size(); i += 1) {
-            int N = Ns.get(i);
+        for (int i = 0; i < ns.size(); i += 1) {
+            int N = ns.get(i);
             double time = times.get(i);
             int opCount = opCounts.get(i);
             double timePerOp = time / opCount * 1e6;
@@ -22,24 +23,24 @@ public class TimeADeque {
     }
 
     public static void timeAListConstruction() {
-        int[] NsArray = new int[]{1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000};
-        AList<Integer> Ns = new AList<>();
+        int[] nsArray = new int[]{1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000};
+        AList<Integer> ns = new AList<>();
         AList<Double> times = new AList<>();
         AList<Integer> opCounts = new AList<>();
 
-        for (int n : NsArray) {
+        for (int n : nsArray) {
             ArrayDeque<Integer> ad = new ArrayDeque<>();
 
             Stopwatch sw = new Stopwatch();
             randomized(ad, n);
             double timeInSeconds = sw.elapsedTime();
 
-            Ns.addLast(n);
+            ns.addLast(n);
             times.addLast(timeInSeconds);
             opCounts.addLast(n);
         }
 
-        printTimingTable(Ns, times, opCounts);
+        printTimingTable(ns, times, opCounts);
     }
 
     private static void randomized(ArrayDeque<Integer> ad, int n) {
