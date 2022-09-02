752e36e577c98cad2d55c9650cb0261b6e25db74
rjun <rjun0729@gmail.com>
2022-09-03 07:31:08 +0800

    Commit `TestArrayDequeEC.java` in proj1 EC

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,54 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void randomizedTest() {
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        String msg = "";
+
+        int N = 500;
+        for (int i = 0; i < N; i++) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addFirst
+                int randVal = StdRandom.uniform(0, 100);
+                solution.addFirst(randVal);
+                student.addFirst(randVal);
+
+                msg += "addFirst(" + randVal + ")\n";
+
+                assertEquals(msg, solution.get(0), student.get(0));
+            } else if (operationNumber == 1) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                solution.addLast(randVal);
+                student.addLast(randVal);
+
+                msg += "addLast(" + randVal + ")\n";
+
+                assertEquals(msg, solution.get(solution.size() - 1), student.get(student.size() - 1));
+            } else if (operationNumber == 2 && solution.size() > 0 && student.size() > 0) {
+                // removeFirst
+
+                msg += "removeFirst()\n";
+
+                assertEquals(msg, solution.removeFirst(), student.removeFirst());
+            } else if (operationNumber == 3 && solution.size() > 0 && student.size() > 0) {
+                // removeLast
+
+                msg += "removeLast()\n";
+
+                assertEquals(msg, solution.removeLast(), student.removeLast());
+            }
+        }
+
+    }
+}
