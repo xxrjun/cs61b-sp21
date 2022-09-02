16524566dac27b6fb904b766b71a629071fd9f82
rjun <rjun0729@gmail.com>
2022-09-02 17:34:01 +0800

    Fixed code style problems in `AList.java`.

--- proj1/timingtest/AList.java
+++ proj1/timingtest/AList.java
@@ -1,7 +1,9 @@
 package timingtest;
 
-/** Array based list.
- *  @author Josh Hug
+/**
+ * Array based list.
+ *
+ * @author Josh Hug
  */
 
 //         0 1  2 3 4 5 6 7
@@ -18,20 +20,26 @@ public class AList<Item> {
     private Item[] items;
     private int size;
 
-    /** Creates an empty list. */
+    /**
+     * Creates an empty list.
+     */
     public AList() {
         items = (Item[]) new Object[100];
         size = 0;
     }
 
-    /** Resizes the underlying array to the target capacity. */
+    /**
+     * Resizes the underlying array to the target capacity.
+     */
     private void resize(int capacity) {
         Item[] a = (Item[]) new Object[capacity];
         System.arraycopy(items, 0, a, 0, size);
         items = a;
     }
 
-    /** Inserts X into the back of the list. */
+    /**
+     * Inserts X into the back of the list.
+     */
     public void addLast(Item x) {
         if (size == items.length) {
             resize((int) (size * 1.37));
@@ -41,22 +49,31 @@ public class AList<Item> {
         size = size + 1;
     }
 
-    /** Returns the item from the back of the list. */
+    /**
+     * Returns the item from the back of the list.
+     */
     public Item getLast() {
         return items[size - 1];
     }
-    /** Gets the ith item in the list (0 is the front). */
+
+    /**
+     * Gets the ith item in the list (0 is the front).
+     */
     public Item get(int i) {
         return items[i];
     }
 
-    /** Returns the number of items in the list. */
+    /**
+     * Returns the number of items in the list.
+     */
     public int size() {
         return size;
     }
 
-    /** Deletes item from back of the list and
-      * returns deleted item. */
+    /**
+     * Deletes item from back of the list and
+     * returns deleted item.
+     */
     public Item removeLast() {
         Item x = getLast();
         items[size - 1] = null;
